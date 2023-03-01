b24753ff3caf4d727fd00551ad91d25ae9d16192
bhuvesh <bhuvesh@createbytes.com>
2023-03-01 09:13:37 +0530

    dishpoint started

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Menu } from "semantic-ui-react";
+import { Link, NavLink } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <Menu borderless fixed="top">
+      <Menu.Item>
+        <Menu.Header style={{ marginRight: "25px" }}>
+          <h1>DISHPOINT</h1>
+        </Menu.Header>
+      </Menu.Item>
+      <Menu.Item as={Link} to="/" content="Home" />
+      <Menu.Item as={NavLink} to="/recipes" content="Recipes" />
+    </Menu>
+  );
+}
+export default Navbar;
